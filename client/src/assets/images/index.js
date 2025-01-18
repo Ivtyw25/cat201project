3bffb83bbe3d6cce7d45f19c99226cf456c6fea1
Yuan-0210 <houyuan0210@gmail.com>
2025-01-19 06:23:23 +0800

    feat: add images

--- client/src/assets/images/index.js
+++ client/src/assets/images/index.js
@@ -1,5 +1,113 @@
 import HeroImage from "./pokemontradingpack.jpg"
+import Aizawa from "./aizawa.jpg"
+import AllForOne from "./all_for_one.jpg"
+import AllMight from "./all_might.png"
+import Applin from "./applin.png"
+import Armin from "./armin.jpg"
+import ArmoredTitan from "./armored_titan.jpg"
+import BoaHancock from "./boa_hancock.jpg"
+import Charista from "./charista.jpg"
+import ChimiAnnie from "./chimi_annie.jpg"
+import ChimiEren from "./chimi_eren.jpg"
+import ChimiMikasa from "./chimi_mikasa.jpg"
+import Claceon from "./claceon.png"
+import Eren from "./eren.jpg"
+import Espeon from "./espeon.png"
+import FemaleTitan from "./female_titan.jpg"
+import FlareonEx from "./flareon_ex.png"
+import Frog from "./frog.jpg"
+import Goldeen from "./goldeen.png"
+import HarunoSakuraCr from "./haruno_sakura_cr.jpg"
+import HinataHyugaNr from "./hinata_hyuga_nr.jpg"
+import Hydrapple from "./hydrapple.png"
+import IronHandsEx from "./iron_hands_ex.png"
+import IronThornsEx from "./iron_thorns_ex.png"
+import IzukuMidoriya from "./izuku_midoriya.jpg"
+import JiraiyaNr from "./jiraiya_nr.jpg"
+import Jolteon from "./jolteon.jpg"
+import KatsukiBakugo from "./katsuki_bakugo.jpg"
+import Kirishima from "./kirishima.jpg"
+import KuokaJiro from "./kuoka_jiro.jpg"
+import KushinaUzumakiNr from "./kushina_uzumaki_nr.jpg"
+import LeafeonEx from "./leafeon_ex.png"
+import Levi from "./levi.jpg"
+import Litleo from "./litleo.png"
+import Mikasa from "./mikasa.jpg"
+import MonekyDLuffy from "./monkey_d_luffy.jpg"
+import NarutoNr from "./naruto_nr.jpg"
+import NarutoUzumakiCr from "./naruto_uzumaki_cr.jpg"
+import Perona from "./perona.jpg"
+import PikachuEx from "./pikachu_ex.png"
+import Pyroar from "./pyroar.png"
+import Rebecca from "./rebecca.jpg"
+import RoronoaZoro from "./roronoa_zoro.jpg"
+import SasukeUchihaCr from "./sasuke_uchiha_cr.jpg"
+import Shanks from "./shanks.jpg"
+import Shigaraki from "./shigaraki.jpg"
+import Slowking from "./slowking.png"
+import Suicune from "./suicune.png"
+import Todoroki from "./todoroki.jpg"
+import TonyChopper from "./tony_chopper.jpg"
+import TrafalgarLaw from "./trafalgar_law.jpg"
+import TsunadeNr from "./tsunade_nr.jpg"
+import UchihcaMadaraNr from "./uchiha_madara_nr.jpg"
+import UchihaObitoBp from "./uchiha_obito_bp.jpg"
+import Yamato from "./yamato.jpg"
 
 export {
     HeroImage,
+    Aizawa,
+    AllForOne,
+    AllMight,
+    Applin,
+    Armin,
+    ArmoredTitan,
+    BoaHancock,
+    Charista,
+    ChimiAnnie,
+    ChimiEren,
+    ChimiMikasa,
+    Claceon,
+    Eren,
+    Espeon,
+    FemaleTitan,
+    FlareonEx,
+    Frog,
+    Goldeen,
+    HarunoSakuraCr,
+    HinataHyugaNr,
+    Hydrapple,
+    IronHandsEx,
+    IronThornsEx,
+    IzukuMidoriya,
+    JiraiyaNr,
+    Jolteon,
+    KatsukiBakugo,
+    Kirishima,
+    KuokaJiro,
+    KushinaUzumakiNr,
+    LeafeonEx,
+    Levi,
+    Litleo,
+    Mikasa,
+    MonekyDLuffy,
+    NarutoNr,
+    NarutoUzumakiCr,
+    Perona,
+    PikachuEx,
+    Pyroar,
+    Rebecca,
+    RoronoaZoro,
+    SasukeUchihaCr,
+    Shanks,
+    Shigaraki,
+    Slowking,
+    Suicune,
+    Todoroki,
+    TonyChopper,
+    TrafalgarLaw,
+    TsunadeNr,
+    UchihcaMadaraNr,
+    UchihaObitoBp,
+    Yamato,
 }
